ea7ad24ce0f5aeb3c2ab92e1a2da81d76383a9e3
slavillacwt <slavilla@mycwt.com>
2023-04-27 17:46:03 +0800

    Initial commit

---
+++ src/main/java/com/example/SQSTest/SqsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.SQSTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SQSTest/SqsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SQSTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
